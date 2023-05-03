0fb9e16eb66999ef4b3635624523f635d7584df7
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-05-03 16:02:38 -0300

    feat: configuring the movieService and adding the home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private service: MovieApiServiceService) {}
 
+  bannerResults: any = [];
+
+  ngOnInit(): void {
+    this.bannerData();
+  }
+  bannerData() {
+    this.service.bannerApiData().subscribe((result) => {
+      this.bannerResults = result.results;
+    });
+  }
 }
