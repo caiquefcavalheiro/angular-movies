81e61d3284f05c5d50c8c5eec2440f5bdfe0db89
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-05-06 15:42:32 -0300

    feat: Added movie search function and linked to search component to perform search on button click

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { MovieApiServiceService } from 'src/app/service/movie-api-service.service';
 
 @Component({
   selector: 'app-search',
@@ -7,6 +8,7 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
+  constructor(private service: MovieApiServiceService) {}
   ngOnInit(): void {}
 
   searchForm = new FormGroup({
@@ -14,6 +16,8 @@ export class SearchComponent implements OnInit {
   });
 
   submitForm() {
-    console.log(this.searchForm.value, 'searchForm#');
+    this.service.getSearchMovie(this.searchForm.value).subscribe((result) => {
+      console.log(result, 'SearchMovie#');
+    });
   }
 }
--- src/app/service/movie-api-service.service.ts
+++ src/app/service/movie-api-service.service.ts
@@ -21,4 +21,10 @@ export class MovieApiServiceService {
       `${this.baseUrl}/trending/movie/day?api_key=${this.apiKey}`
     );
   }
+
+  getSearchMovie(data: any): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}/search/movie?api_key=${this.apiKey}&query=${data.movieName}`
+    );
+  }
 }
