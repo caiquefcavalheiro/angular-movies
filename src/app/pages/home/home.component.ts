a69c6c0994dde89b1b1fb87391c2c611809cb178
Caique F. Cavalheiro <caiquefcavalheiro@gmail.com>
2023-05-07 00:11:01 -0300

    feat: added functions to capture movies by categories and added on home screen

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,10 +11,24 @@ export class HomeComponent implements OnInit {
 
   bannerResults: any = [];
   tredingMovieResult: any = [];
+  actionMovieResult: any = [];
+  adventureMovieResult: any = [];
+  animationMovieResult: any = [];
+  comedyMovieResult: any = [];
+  documentaryMovieResult: any = [];
+  scienceFictionMovieResult: any = [];
+  thrillerMovieResult: any = [];
 
   ngOnInit(): void {
     this.bannerData();
     this.tredingData();
+    this.actionMovie();
+    this.adventureMovie();
+    this.animationMovie();
+    this.comedyMovie();
+    this.documentaryMovie();
+    this.scienceFictionMovie();
+    this.thrillerMovie();
   }
   bannerData() {
     this.service.bannerApiData().subscribe((result) => {
@@ -25,6 +39,47 @@ export class HomeComponent implements OnInit {
   tredingData() {
     this.service.tredingMovieApiData().subscribe((result) => {
       this.tredingMovieResult = result.results;
+    });
+  }
+  actionMovie() {
+    this.service.fetchActionMovies().subscribe((result) => {
+      this.actionMovieResult = result.results;
+    });
+  }
+
+  adventureMovie() {
+    this.service.fetchAdventureMovies().subscribe((result) => {
+      this.adventureMovieResult = result.results;
+    });
+  }
+
+  animationMovie() {
+    this.service.fetchAnimationMovies().subscribe((result) => {
+      this.animationMovieResult = result.results;
+    });
+  }
+
+  comedyMovie() {
+    this.service.fetchComedyMovies().subscribe((result) => {
+      this.comedyMovieResult = result.results;
+    });
+  }
+
+  documentaryMovie() {
+    this.service.fetchDocumentaryMovies().subscribe((result) => {
+      this.documentaryMovieResult = result.results;
+    });
+  }
+
+  scienceFictionMovie() {
+    this.service.fetchScienceFictionMovies().subscribe((result) => {
+      this.scienceFictionMovieResult = result.results;
+    });
+  }
+
+  thrillerMovie() {
+    this.service.fetchThrillerMovies().subscribe((result) => {
+      this.thrillerMovieResult = result.results;
     });
   }
 }
--- src/app/service/movie-api-service.service.ts
+++ src/app/service/movie-api-service.service.ts
@@ -27,4 +27,64 @@ export class MovieApiServiceService {
       `${this.baseUrl}/search/movie?api_key=${this.apiKey}&query=${data.movieName}`
     );
   }
+
+  getMovieDetails(data: any): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}/movie/${data}?api_key=${this.apiKey}`
+    );
+  }
+
+  getMovieVideo(data: any): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}/movie/${data}/videos?api_key=${this.apiKey}`
+    );
+  }
+
+  getMovieCast(data: any): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}/movie/${data}/credits?api_key=${this.apiKey}`
+    );
+  }
+
+  fetchActionMovies(): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}/discover/movie?api_key=${this.apiKey}&with_genres=28`
+    );
+  }
+
+  fetchAdventureMovies(): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}/discover/movie?api_key=${this.apiKey}&with_genres=12`
+    );
+  }
+
+  fetchAnimationMovies(): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}/discover/movie?api_key=${this.apiKey}&with_genres=16`
+    );
+  }
+
+  fetchComedyMovies(): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}/discover/movie?api_key=${this.apiKey}&with_genres=35`
+    );
+  }
+
+  fetchDocumentaryMovies(): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}/discover/movie?api_key=${this.apiKey}&with_genres=99`
+    );
+  }
+
+  fetchScienceFictionMovies(): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}/discover/movie?api_key=${this.apiKey}&with_genres=878`
+    );
+  }
+
+  fetchThrillerMovies(): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}/discover/movie?api_key=${this.apiKey}&with_genres=53`
+    );
+  }
 }
